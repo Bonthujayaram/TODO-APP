bb134c39b0ef7069850d7e1a4b1a276d01d99c85
Bontuhjayaram <bonthujayaram57@gmail.comm>
2025-05-29 02:08:21 +0530

    Initial commit

---
+++ src/TodoForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const TodoForm = ({ onAdd }) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === '') {
+      alert('Todo cannot be empty!');
+      return;
+    }
+    onAdd(text);
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-box">
+      <span className="icon">
+        {/* You can use any icon library like FontAwesome or Ionicons here */}
+        <ion-icon name="add-circle-outline"></ion-icon>
+      </span>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        required
+      />
+      <label>Todo</label>
+    </div>
+      <button type="submit">Add Todo</button>
+    </form>
+    
+  );
+};
+
+export default TodoForm;
